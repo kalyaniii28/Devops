429f6c54b2d400242593714cf8981c517f6c1e91
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-01-11 01:56:30 +0530

    Update main.tf

--- remote state/main.tf
+++ remote state/main.tf
@@ -1,1 +1,17 @@
+terraform {
+required_version = ">= 0.12"
+}
+
+provider "aws"{}
+
+data "aws_caller_identity" "current" {}
+
+local{
+account_id = data.aws_caller_identity.current.accountid #mention account id on details
+}
+
+resource "aws_s3_bucket" "example"{
+backet = "${local.account_id)-terraform-state"
+ version
+
 
