48f697b5acb5a283b850fe7a835701eb0fb89f34
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-01-11 13:54:55 +0530

    Update output.tf

--- remote state/output.tf
+++ remote state/output.tf
@@ -3,23 +3,22 @@ output "s3_bucket_name" {
   description = "The NAME of the S3 bucket"
 }
 
-output "s3_name_arn"{
-  value =aws_s3_arn.terraform_state.arn
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
   description = "The ARN of the S3 bucket"
 }
 
-output "s3_name_region"{
-  value =aws_s3_riegon.terraform_state.arn
-  description = "The region of the S3 bucket"
+output "s3_bucket_region" {
+  value       = aws_s3_bucket.terraform_state.region
+  description = "The REGION of the S3 bucket"
 }
 
-output "dynamodb_table_name"{
-  value =dynamodb_table_name.terraform_lock.name
-  description = "The name of the tablt"
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_lock.name
+  description = "The ARN of the DynamoDB table"
 }
 
-output "dynamodb_table_arn"{
-  value =dynamodb_table_arn.terraform_lock.arn
-  description = "The arn of the tablt"
+output "dynamodb_table_arn" {
+  value       = aws_dynamodb_table.terraform_lock.arn
+  description = "The ARN of the DynamoDB table"
 }
-
