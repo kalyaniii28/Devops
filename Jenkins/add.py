a286d27fdce2f0bd5309d79de697185e10abf9bc
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-01-11 20:31:21 +0530

    Update add.py

--- Jenkins/add.py
+++ Jenkins/add.py
@@ -1,6 +1,10 @@
-# addition.py
-a = 5
-b = 3
-result = a + b
-print(f"The result of {a} + {b} is {result}")
+# add.py
+import os
 
+# Get the numbers from environment variables
+num1 = float(os.getenv("NUM1", 0))
+num2 = float(os.getenv("NUM2", 0))
+
+# Perform addition
+result = num1 + num2
+print(f"The result of {num1} + {num2} is {result}")
