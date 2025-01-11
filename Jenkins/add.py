fdad9e1fc5b4b763c67c72402e016b8cd9e034f2
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-01-11 20:17:51 +0530

    Update add.py

--- Jenkins/add.py
+++ Jenkins/add.py
@@ -1,12 +1,6 @@
-# Python program to add two numbers
-
-# Input: Take two numbers as input from the user
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-# Addition of two numbers
-sum_result = num1 + num2
-
-# Output: Display the result
-print(f"The sum of {num1} and {num2} is: {sum_result}")
+# addition.py
+a = 5
+b = 3
+result = a + b
+print(f"The result of {a} + {b} is {result}")
 
