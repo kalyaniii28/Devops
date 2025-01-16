cd29722975f9fe75d0de7023cc2b63785a5ab5bd
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-01-16 18:44:32 +0530

    .

--- remote state/main.tf
+++ remote state/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = ">= 0.12"
 
@@ -7,40 +6,40 @@ terraform {
   }
 }
 
-# ------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ 
+# CONFIGURE OUR AWS CONNECTION 
+# ------------------------------------------------------------------------------ 
 
 provider "aws" {}
 
 data "aws_caller_identity" "current" {}
 
-locals{
+locals {
   account_id = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${local.account_id}-terraform-states"
 
-  versioning{
+  versioning {
     enabled = true
   }
 
-  server_side_encryption_configuration{
+  server_side_encryption_configuration {
     rule {
-      apply_server_side_encryption_by_default{
+      apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
     }
   }
-  resource "aws_dynamodb_table" "terraform_lock" {
-  billing_mode     = "PAY_PER_REQUEST"
-  hash_key         = "TestTableHashKey"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockId"
 
   attribute {
-    name = "TestTableHashKey"
+    name = "LockId"
     type = "S"
   }
-  }
-
-
+}
