152cb1e36df4ff93e3d106d7919b2b9f40b3b128
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-02-05 00:57:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@
 set -e
 
 docker pull kalyaniii28/python-app:latest
-docker run -d -p 5000:5000 kalayniii28/python-app:latest
+docker run -d -p 5000:5000 kalyaniii28/python-app:latest
 
 echo " docker run sucess"
