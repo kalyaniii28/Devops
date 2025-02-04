ec8118ed05c39a8575ad6762e4910012fb2ccae4
kalyaniii28 <kalyanii.bashetti@gmail.com>
2025-02-05 01:27:22 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "Deleting existing Docker containers"
 containerid=$(docker ps -q)  # Capture the container IDs into the variable
-docker rm -f $containerid  # Force-remove the containers
+docker rm -f $containerid  # Force-remove the containerss
 
